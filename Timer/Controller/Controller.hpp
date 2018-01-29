f419d326b8d1f128b8c69e97595193463c29c80a
ConnorOLeary <themidlvlboss@gmail.com>
2018-01-29 09:01:11 -0700

    Created the timer project

---
+++ Timer/Controller/Controller.hpp
@@ -0,0 +1,16 @@
+//
+//  Controller.hpp
+//  Timer
+//
+//  Created by Oleary, CONNOR on 1/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <time.h>
+#include <assert.h>
+#include <iostream>
+
+#endif /* Controller_hpp */
